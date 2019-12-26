79022adcb6a75a3f2d8d6e1ea8b871c6a6d8f4b0
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2019-12-27 01:59:33 +0200

    MVC Project initial commit

---
+++ Movie_Rental/Controllers/MoviesController.cs
@@ -0,0 +1,19 @@
+﻿using Movie_Rental.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Movie_Rental.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public ActionResult Random()
+        {
+            var movie = new Movie { Name = "Seven" };
+
+            return View(movie);
+        }
+    }
+}
