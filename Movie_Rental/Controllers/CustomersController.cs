95dd6ceb8353437563081bdcc811cb1525dc0c63
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2019-12-27 22:34:37 +0200

    Editing form

--- Movie_Rental/Controllers/CustomersController.cs
+++ Movie_Rental/Controllers/CustomersController.cs
@@ -41,17 +41,46 @@ namespace Movie_Rental.Controllers
             var membershipTypes = _context.MembershipTypes.ToList();
             var viewModel = new NewCustomerViewModel { MembershipTypes = membershipTypes };
 
-            return View(viewModel);
+            return View("CustomerForm", viewModel);
         }
 
         [HttpPost]
-        public ActionResult Create(Customer customer) 
+        public ActionResult Save(Customer customer) 
         {
-            _context.Customers.Add(customer);
+            if (customer.Id == 0)
+            {
+                _context.Customers.Add(customer);
+            }
+            else
+            {
+                var customerInDb = _context.Customers.Single(c => c.Id == customer.Id);
+                customerInDb.Name = customer.Name;
+                customerInDb.BirthDate = customer.BirthDate;
+                customerInDb.MembershipTypeId = customer.MembershipTypeId;
+                customerInDb.IsSubscribedToNewsletter = customer.IsSubscribedToNewsletter;
+            }
+            
             _context.SaveChanges();
 
             return RedirectToAction("Index","Customers");
         }
+
+        public ActionResult Edit(int id)
+        {
+            var customer = _context.Customers.FirstOrDefault(c => c.Id == id);
+
+            if (customer == null) return HttpNotFound();
+
+            var viewModel = new NewCustomerViewModel { Customer = customer, MembershipTypes = _context.MembershipTypes.ToList() };
+
+            return View("CustomerForm", viewModel);
+        }
+
+
+
+
+
+
 
         //private IEnumerable<Customer> GetCustomers()
         //{
