c9b0678e66020262932517b6f6d3a1893b27a1a1
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2019-12-27 17:16:36 +0200

    EF Seed database,Annotations,Querying

--- Movie_Rental/Controllers/CustomersController.cs
+++ Movie_Rental/Controllers/CustomersController.cs
@@ -4,34 +4,51 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
 
 namespace Movie_Rental.Controllers
 {
     public class CustomersController : Controller
-    {       
+    {
+        private readonly ApplicationDbContext _context;
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
         public ActionResult Index()
         {
-            var customers = GetCustomers();
+            //var customers = GetCustomers();
+
+            //Using EF
+            var customers = _context.Customers.Include(c => c.MembershipType).ToList();
 
             return View(customers);
         }
 
         public ActionResult Details(int id)
         {
-            var customer = GetCustomers().FirstOrDefault(c => c.Id == id);
+            //var customer = GetCustomers().FirstOrDefault(c => c.Id == id);
+
+            //Using EF
+            var customer = _context.Customers.FirstOrDefault(c => c.Id == id);
+
             if (customer == null) return HttpNotFound();
 
             return View(customer);
         }
 
-        private IEnumerable<Customer> GetCustomers()
-        {
-            return new List<Customer>
-            {
-                new Customer{Id = 1 , Name = "Ted"},
-                new Customer{Id = 2 , Name = "Kostas"},
-                new Customer{Id = 3 , Name = "Maria"}
-            };
-        }
+        //private IEnumerable<Customer> GetCustomers()
+        //{
+        //    return new List<Customer>
+        //    {
+        //        new Customer{Id = 1 , Name = "Ted"},
+        //        new Customer{Id = 2 , Name = "Kostas"},
+        //        new Customer{Id = 3 , Name = "Maria"}
+        //    };
+        //}
     }
 }
