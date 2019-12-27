49f55d4abd15d028812dc84b5f24647960761ead
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2019-12-27 16:25:56 +0200

    Entity Framework setup and learning the basics

---
+++ Movie_Rental/Migrations/201912271415427_AddIsSubscribedToCustomers.cs
@@ -0,0 +1,18 @@
+namespace Movie_Rental.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddIsSubscribedToCustomers : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Customers", "IsSubscribedToNewsletter", c => c.Boolean(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Customers", "IsSubscribedToNewsletter");
+        }
+    }
+}
