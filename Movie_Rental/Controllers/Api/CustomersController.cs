33855851fa22fbe1cc4cc2e9473ff141d0fbeec9
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2019-12-28 12:52:28 +0200

    Building the Customer REST API

---
+++ Movie_Rental/Controllers/Api/CustomersController.cs
@@ -0,0 +1,70 @@
+﻿using Movie_Rental.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Movie_Rental.Controllers.Api
+{
+    public class CustomersController : ApiController
+    {
+        private readonly ApplicationDbContext _context;
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        public IEnumerable<Customer> GetCustomers()
+        {
+            return _context.Customers.ToList();
+        }
+
+        public Customer GetCustomer(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customer == null) throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            return customer;
+        }
+
+        [HttpPost]
+        public Customer CreateCustomer(Customer customer)
+        {
+            if (!ModelState.IsValid) throw new HttpResponseException(HttpStatusCode.BadRequest);
+
+            _context.Customers.Add(customer);
+            _context.SaveChanges();
+
+            return customer;
+        }
+
+        [HttpPut]
+        public void UpdateCustomer(int id, Customer customer)
+        {
+            if (!ModelState.IsValid) throw new HttpResponseException(HttpStatusCode.BadRequest);
+
+            var custInDb = _context.Customers.FirstOrDefault(c => c.Id == id);
+            if (custInDb == null) throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            custInDb.Name = customer.Name;
+            custInDb.BirthDate = customer.BirthDate;
+            custInDb.IsSubscribedToNewsletter = customer.IsSubscribedToNewsletter;
+            custInDb.MembershipTypeId = customer.MembershipTypeId;
+
+            _context.SaveChanges();
+        }
+
+        [HttpDelete]
+        public void DeleteCustomer(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customer == null) throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            _context.Customers.Remove(customer);
+            _context.SaveChanges();
+        }
+    }
+}
