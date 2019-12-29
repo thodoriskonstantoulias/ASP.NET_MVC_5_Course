00a294a7f8ff80645fdca214f53a28aa00144f36
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2019-12-29 12:46:33 +0200

    Rentals functionality Part 1 - Creation of table and simple API

---
+++ Movie_Rental/Controllers/Api/NewRentalController.cs
@@ -0,0 +1,41 @@
+﻿using Movie_Rental.Dtos;
+using Movie_Rental.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Movie_Rental.Controllers.Api
+{
+    public class NewRentalController : ApiController
+    {
+        private readonly ApplicationDbContext _context;
+        public NewRentalController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        public IHttpActionResult CreateNewRentals(NewRentalDto newRental)
+        {
+            var customer = _context.Customers.Single(c => c.Id == newRental.CustomerId);
+            var movies = _context.Movies.Where(m => newRental.MoviesIds.Contains(m.Id));
+            foreach (var movie in movies)
+            {
+                var rental = new Rental
+                {
+                    Customer = customer,
+                    Movie = movie,
+                    DateRented = DateTime.Now
+                };
+
+                _context.Rentals.Add(rental);
+            }
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
--- Movie_Rental/Models/IdentityModels.cs
+++ Movie_Rental/Models/IdentityModels.cs
@@ -33,6 +33,8 @@ namespace Movie_Rental.Models
         public DbSet<Movie> Movies { get; set; }
         public DbSet<MembershipType> MembershipTypes { get; set; }
         public DbSet<MovieGenre> MovieGenres { get; set; }
+        public DbSet<Rental> Rentals { get; set; }
+
 
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
